c9d9e5a8a4f9491203fd3f59e7c2378486ad5e11
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-05-20 14:48:29 +0100

    Implement archive folder create validation and tests

--- src/main/java/dz/procsys/api/core/procurement/archive/repository/ArchiveFolderRepository.java
+++ src/main/java/dz/procsys/api/core/procurement/archive/repository/ArchiveFolderRepository.java
@@ -4,4 +4,5 @@ import dz.procsys.api.core.procurement.archive.model.ArchiveFolder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArchiveFolderRepository extends JpaRepository<ArchiveFolder, Long> {
+    boolean existsByCode(String code);
 }
